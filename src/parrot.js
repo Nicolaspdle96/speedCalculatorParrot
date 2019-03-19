58b8261a4f863f0421bf77cfde95e7df76b671bc
Nicolas Ponce de Leon <nicolaspdle@gmail.com>
2019-03-19 09:10:40 -0400

    all test passed

--- src/parrot.js
+++ src/parrot.js
@@ -5,34 +5,22 @@ export const PARROT_TYPES = {
 };
 
 export class Parrot {
-    constructor(type, numberOfCoconuts, voltage, isNailed) {
-        this.loadFactor = 9;
+    constructor(type) {
         this.baseSpeed = 12;
-
         this.type = type;
-        this.numberOfCoconuts = numberOfCoconuts;
-        this.voltage = voltage;
-        this.isNailed = isNailed;
     }
 
     getSpeed() {
-        switch (this.type) {
-
-            case PARROT_TYPES.EUROPEAN:
-                return new European(this.numberOfCoconuts, this.voltage, this.isNailed).getSpeed();
-            case PARROT_TYPES.AFRICAN:
-                return new African(this.numberOfCoconuts, this.voltage, this.isNailed).getSpeed();
-            case PARROT_TYPES.NORWEGIAN_BLUE:
-                return new Norwegian_Blue(this.numberOfCoconuts, this.voltage, this.isNailed).getSpeed();
-        }
         throw new Error("Should be unreachable");
     }
 
 }
 
-class African extends Parrot{
-    constructor(numberOfCoconuts, voltage, isNailed) {
-        super("AFRICAN",numberOfCoconuts, voltage, isNailed);
+export class African extends Parrot{
+    constructor(numberOfCoconuts) {
+        super("AFRICAN");
+        this.numberOfCoconuts = numberOfCoconuts;
+        this.loadFactor = 9;
     }
 
     getSpeed() {
@@ -40,9 +28,11 @@ class African extends Parrot{
     }
 }
 
-class Norwegian_Blue extends Parrot{
-    constructor(numberOfCoconuts, voltage, isNailed) {
-        super("NORWEGIAN",numberOfCoconuts, voltage, isNailed);
+export class Norwegian_Blue extends Parrot{
+    constructor(voltage, isNailed) {
+        super("NORWEGIAN");
+        this.voltage = voltage;
+        this.isNailed = isNailed;
     }
 
     getSpeed() {
@@ -53,9 +43,9 @@ class Norwegian_Blue extends Parrot{
     }
 }
 
-class European extends Parrot{
-    constructor(numberOfCoconuts, voltage, isNailed) {
-        super("EUROPEAN",numberOfCoconuts, voltage, isNailed);
+export class European extends Parrot{
+    constructor() {
+        super("EUROPEAN");
     }
 
     getSpeed() {
